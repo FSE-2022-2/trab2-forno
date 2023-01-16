38fde2ec1312cc054b2ed2036fd508c2f2c1e69b
GuilhermeBraz <gvcerveirabraz@gmail.com>
2023-01-16 15:49:33 -0300

    modularize uart-modbus

---
+++ inc/uart-modbus.h
@@ -0,0 +1,9 @@
+#ifndef UART_MODBUS_H
+#define UART_MODBUS_H
+
+int open_uart(int uart0_filestream);
+void close_uart(int uart0_filestream);
+int write_uart(int uart0_filestream, unsigned char *tx_buffer, unsigned char *p_tx_buffer);
+int read_uart(int uart0_filestream);
+
+#endif
